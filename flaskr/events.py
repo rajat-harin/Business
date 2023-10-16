8a62910a4bbc8a083962d3b1754732137f2e5e95
rajat-harin <rajat.harinkhede@gmail.com>
2023-10-16 17:10:47 +0530

    Player Movement done. static for 2 players

--- flaskr/events.py
+++ flaskr/events.py
@@ -6,7 +6,7 @@ from . import socketio
 bp = Blueprint('event', __name__)
 
 players = dict()
-#player = {'name': '', 'room':'', 'location': 0, 'balance': 0, 'avatar' : '', turn: 0}
+#player = {'name': '', 'room':'', 'location': 0, 'balance': 0, 'avatar' : '', turn: 0, turnPlayed: False}
 rooms = list()
 currentTurn = 0
 
@@ -25,6 +25,7 @@ def joined(data):
             break
         players[player]['avatar'] = 'avatar'+ str(counter) +'.png'
         players[player]['turn'] = counter
+        players[player]['turnPlayed'] = False
         counter += 1
 
     join_room(room)
@@ -34,12 +35,37 @@ def joined(data):
 @socketio.on('diceRolling')
 def diceRolling(data):
     room = session.get('room')
-    number = random.randrange(1,13)
-    print(data)
-    emit('playerMove',{'sourceLocation':data['location'], 'targetLocation':(data['location']+number)%40},room=room)
-    emit('diceRolled',{'number':number},room=room)
+    print(currentTurn)
+
+    if players[data['player']]['turn'] == currentTurn and not players[data['player']]['turnPlayed']:
+        players[data['player']]['turnPlayed'] = True
+        roll = random.randrange(1,13)
+        if players[data['player']]['location'] // 40 == 1:
+            players[data['player']]['location'] = (players[data['player']]['location'] + roll) % 40
+            updateBalance(data['player'], expense=0)
+        else:
+            players[data['player']]['location'] = (players[data['player']]['location'] + roll) % 40
+        emit('playerMove',{'players':players,'sourceLocation':data['location'], 'targetLocation':(data['location']+roll)%40},room=room)
+        emit('diceRolled',{'number':roll},room=room)
+    else:
+        emit('status', {'msg': session.get('name') + ' dont be greedy. wait for your turn'}, room=room)
+
+@socketio.on('finishTurn')
+def finishTurn(data):
+    room = session.get('room')
+    global currentTurn
+    expense = 0
+    if players[data['player']]['turn'] == currentTurn and players[data['player']]['turnPlayed']:
+        players[data['player']]['turnPlayed'] = False
+        currentTurn = (currentTurn + 1) % 2 #chage to players in room
+        updateBalance(data['player'], expense)
+    print(currentTurn)
 
 def checkTurn(playerTurn):
     if playerTurn == currentTurn :
         return True
     return False
+
+def updateBalance(player, expense):
+    #update balace based on player activity
+    pass
--- flaskr/static/game.js
+++ flaskr/static/game.js
@@ -16,16 +16,23 @@ socket.on("connect", () => {
 socket.on("disconnect", () => {
     console.log('disconnected!')
 });
+
+socket.on('status', (data) => {
+    $('#chat').val($('#chat').val() + '<' + data.msg + '>\n');
+    $('#chat').scrollTop($('#chat')[0].scrollHeight);
+});
+
 socket.on("diceRolled", (data) => {
     $("#Dice").text(data.number)
 });
 socket.on("setPlayers", (data) => {
     console.log("setting Players!");
     $("#b0").html(``)
+    playersState = data.players
     newPlayersState = data.players
     for (player in data.players) {
         $("#b" + data.players[player].location)
-            .append(`<img id="player1" class="avatar" src = "/static/${data.players[player].avatar}" />`)
+            .append(`<img id="${player}" class="avatar" src = "/static/${data.players[player].avatar}" />`)
         // if (Object.hasOwnProperty.call(object, key)) {
         //     const element = object[key];
 
@@ -33,9 +40,8 @@ socket.on("setPlayers", (data) => {
     };
 });
 socket.on("playerMove", (data) => {
-    console.log("moving Player!");
-    $("#b" + data.sourceLocation + ">#player1").detach().prependTo("#b" + data.targetLocation);
-    playerLocation[0] = data.targetLocation
+    console.log("moving player!")
+    updateBoard(playersState, data.players)
 });
 function rollDice() {
     $("#Dice").text("Rolling...")
@@ -44,4 +50,24 @@ function rollDice() {
 
 function updateBoard(playersState, newPlayersState) {
     // update board based on current state of players object
+    console.log("checking Player state difference!")
+    console.log(playersState);
+    console.log(newPlayersState);
+    for(player in playersState) {
+        if(playersState[player].location != newPlayersState[player].location) {
+            updateLocation(player, playersState[player].location, newPlayersState[player].location)
+            playersState[player].location = newPlayersState[player].location
+        }
+    }
+}
+
+function updateLocation(player, source, target) {
+    //update location of player avatar
+    console.log("updating Location!");
+    $("#b" + source + `>#${player}`).detach().prependTo("#b" + target);
+}
+
+function finishTurn() {
+    $("#Dice").text("finishing...")
+    socket.emit("finishTurn", { player: playerName, location: playerLocation[0] });
 }
